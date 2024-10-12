73e9f892bae26d44526e027cb0b83f13588672d2
SyedSamad7 <syedsamadshah15@gmail.com>
2024-10-12 01:02:31 -0700

    initial commit

---
+++ built_in_types.dart
@@ -0,0 +1,14 @@
+void main() {
+  // Numbers comes in two sub types (1) [int], (2) [double]
+  int a = 10;
+  double b = 10.0;
+  print('int value $a');
+  print('double value $b');
+
+  //we can declare variable with num type. but in this we can pass both value [int] and double
+  num c = 10;
+  c += 10.0;
+  print('num value $c');
+
+  assert('1' == '2');
+}
