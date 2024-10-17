e81fd7de5b74c236bf3cb312a6d46648def718a5
SyedSamad7 <syedsamadshah15@gmail.com>
2024-10-17 06:08:44 -0700

    changes in generics

--- generics.dart
+++ generics.dart
@@ -1,6 +1,5 @@
 void main() {
-  ExampleWithOutGenerics pair =
-      ExampleWithOutGenerics(first: 2, second: 'second');
+ ExampleOfGenerics<int, String> pair = ExampleOfGenerics(1, 'One');
   ExampleOfGenerics<bool, double> pair2 = ExampleOfGenerics(true, 2.4);
   ExampleOfGenerics pairs3 = ExampleOfGenerics('2', 5);
   pair.display(); // Output: First: 1, Second: One
@@ -8,16 +7,6 @@ void main() {
   pairs3.display();
 }
 
-class ExampleWithOutGenerics {
-  int first;
-  String second;
-
-  ExampleWithOutGenerics({required this.first, required this.second});
-
-  void display() {
-    print('First: $first, Second: $second');
-  }
-}
 
 class ExampleOfGenerics<T, S> {
   T first;
@@ -32,9 +21,3 @@ class ExampleOfGenerics<T, S> {
 }
 
 
-// ExampleOfGenerics<int, String> pair = ExampleOfGenerics(1, 'One');
-//   ExampleOfGenerics<bool, double> pair2 = ExampleOfGenerics(true, 2.4);
-//   ExampleOfGenerics pairs3 = ExampleOfGenerics('2', 5);
-//   pair.display(); // Output: First: 1, Second: One
-//   pair2.display(); // Output: First: true, Second: 2.4
-//   pairs3.display();
